e1d4e834c8ee933e791e0d9c2b0999551b3341f5
Devaswathi <swathidevasenapathy@gmail.com>
2024-06-05 17:16:08 +0530

    Initial commit

---
+++ src/main/java/com/example/bytebliss/ByteblissApplication.java
@@ -0,0 +1,13 @@
+package com.example.bytebliss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteblissApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteblissApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bytebliss/ByteblissApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bytebliss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteblissApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
